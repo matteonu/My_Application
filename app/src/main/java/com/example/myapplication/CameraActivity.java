d29a24607b6ab252007437fa0f952e5aa1bcd510
Greg <rgregory@student.ethz.ch>
2021-03-21 21:02:10 +0100

    added some functionality of the taking picture function but still some errors needed to be fixed

--- app/src/main/java/com/example/myapplication/CameraActivity.java
+++ app/src/main/java/com/example/myapplication/CameraActivity.java
@@ -17,7 +17,30 @@ public class CameraActivity extends Activity {
     private static final Object MEDIA_TYPE_IMAGE = "jpeg";
     private Camera mCamera;
     private CameraPreview mPreview;
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
 
+        private static final String TAG = "RANDOMLY ADDED" ;
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null) {
+                Log.d(TAG, "Error creating media file, check storage permissions");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,20 +53,36 @@ public class CameraActivity extends Activity {
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        // was picture in the tutorial changed it to mPicture
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
     }
 
 
-    /** A safe way to get an instance of the Camera object. */
-    public Camera getCameraInstance(){
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public Camera getCameraInstance() {
         Camera cam = null;
 
-        try{
+        try {
             cam = Camera.open(); //camera Instance
-        }catch(Exception e){
+        } catch (Exception e) {
             //camera in use
         }
 
         return cam;
     }
+
+
 
 }
